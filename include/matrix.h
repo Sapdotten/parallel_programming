011ed109a950f69c6fc3ee503d58dcb61e948a2f
Sapdotten <semenova.a.p.04@gmail.com>
2025-02-25 22:42:29 +0400

    Works saving to file, time count and matrix multiplicating

--- include/matrix.h
+++ include/matrix.h
@@ -4,31 +4,38 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <stdexcept>
 
 namespace lab {
     class Matrix {
-        friend std::ostream& operator<<(std:: ostream& out, const Matrix& matrix);
+        friend std::ostream &operator<<(std::ostream &out, const Matrix &matrix);
+
     private:
         size_t _height;
         size_t _width;
         std::vector<int> _matrix;
 
-        void _fill_random_values(int start = -100, int end = 100);
+        void _fill_random_values(int start = 0, int end = 100);
 
     public:
         Matrix() : _height(0), _width(0) {};
-        explicit Matrix(const std::string& file_path);
+
+        explicit Matrix(const std::string &file_path);
 
         Matrix(size_t height, size_t width, bool isRandomValue);
 
+        size_t Height() const;
 
-        Matrix operator*(const Matrix &right_matrix) const;
+        size_t Width() const;
+
+        Matrix operator*(const Matrix &B) const;
 
         void saveToFile(const std::string &file_path) const;
 
         void readFromFile(const std::string &file_path);
 
         int &operator()(size_t row, size_t column);
+        const int& operator()(const size_t row, const size_t column) const;
 
 
     };
@@ -42,7 +49,8 @@ namespace lab {
             this->_fill_random_values();
         }
     }
-    Matrix::Matrix(const std::string& file_path){
+
+    Matrix::Matrix(const std::string &file_path) {
         this->readFromFile(file_path);
     }
 
@@ -70,19 +78,22 @@ namespace lab {
     void Matrix::readFromFile(const std::string &file_path) {
         std::ifstream in(file_path);
         if (in.is_open()) {
-            in>>this->_width>>this->_height;
-            this->_matrix=std::vector<int>(_width*_height);
-            for(auto& elem: this->_matrix){
-                in>>elem;
+            in >> this->_width >> this->_height;
+            this->_matrix = std::vector<int>(_width * _height);
+            for (auto &elem: this->_matrix) {
+                in >> elem;
             }
         }
     }
 
-    int& Matrix::operator()(const size_t row, const size_t column) {
-        return this->_matrix[row*_width+column];
+    int &Matrix::operator()(const size_t row, const size_t column) {
+        return this->_matrix[row * _width + column];
+    }
+    const int &Matrix::operator()(const size_t row, const size_t column) const {
+        return this->_matrix[row * _width + column];
     }
 
-    std::ostream& operator<<(std:: ostream& out, const Matrix& matrix){
+    std::ostream &operator<<(std::ostream &out, const Matrix &matrix) {
         for (size_t i = 0; i < matrix._height; i++) {
             for (size_t j = 0; j < matrix._width; j++) {
                 out << matrix._matrix[i * matrix._width + j] << " ";
@@ -92,6 +103,32 @@ namespace lab {
         return out;
     }
 
+    size_t Matrix::Height() const {
+        return this->_height;
+    }
+
+    size_t Matrix::Width() const {
+        return this->_width;
+    }
+
+
+    Matrix Matrix::operator*(const lab::Matrix &B) const {
+        if (this->_width != B._height) {
+            throw std::invalid_argument("second matrix should have same count of rows as columns in first matrix.");
+        }
+        Matrix C(this->_height, B._width, false);
+
+        for (int i = 0; i < C._height; i++) {
+            for (int j = 0; j < C._width; j++) {
+                int value = 0;
+                for (int n = 0; n < this->_height; n++) {
+                    value+=this->operator()(i, n)*B(n, j);
+                }
+                C(i, j) = value;
+            }
+        }
+        return C;
+    }
 
 }// namespace lab
 #endif //APP_MATRIX_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,66 @@
 #include <iostream>
 #include <ctime>
 #include "../include/matrix.h"
+#include <string>
+#include <filesystem>
+#include <chrono>
+#include <fstream>
+
+#define CURRENT_TIME std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count()
+
 using namespace lab;
 
-int main(){
-    std::cout<<"Here will be first lab"<< std::endl;
-    std::srand(time(0));
-    Matrix text(4, 4, true);
-    text.saveToFile("example.txt");
-    Matrix check("example.txt");
-    std::cout<<check;
-    std::cout<<check(1, 3);
+void createDirectoryIfNotExists(const std::string& directory) {
+    if (!std::filesystem::exists(directory)) {
+        std::filesystem::create_directory(directory);
+    }
+}
+
+void saveTestResults(const std::string& file_for_time, int n, long long int total_time, int test_count) {
+    std::ofstream out(file_for_time, std::ios::app);
+    if (out.is_open()) {
+        out << n << ";" << total_time / static_cast<double>(test_count) << '\n';
+    } else {
+        std::cerr << "Error opening the file to write results" << std::endl;
+    }
+}
+
+int main() {
+    std::srand(0);
+    std::cout << "Here will be first lab" << std::endl;
+
+    std::string file_for_time = "../data/time_result.csv";
+    std::string data_folder = "../data";
+    std::string matrix_folder = "examples";
+
+    createDirectoryIfNotExists(data_folder);
+    createDirectoryIfNotExists(data_folder + "/" + matrix_folder);
+
+    matrix_folder = data_folder + "/" + matrix_folder;
+    int test_count = 100;
+
+    for (int n = 10, test = 1; n <= 100; n += 10, test++) {
+        std::string test_folder = matrix_folder + "/test_" + std::to_string(test);
+        std::filesystem::create_directory(test_folder);
+
+        long long int sum = 0;
+
+        for (int i = 0; i < test_count; i++) {
+            Matrix A(n, n, true);
+            Matrix B(n, n, true);
+            A.saveToFile(test_folder + "/A");
+            B.saveToFile(test_folder + "/B");
+
+            long long int start = CURRENT_TIME;
+            Matrix C = A * B;
+            long long end = CURRENT_TIME;
+
+            C.saveToFile(test_folder + "/C");
+            sum += (end - start);
+        }
+
+        saveTestResults(file_for_time, n, sum, test_count);
+    }
+
     return 0;
 }
